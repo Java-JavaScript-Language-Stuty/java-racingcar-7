af714360e4f1652a367fe5b861384f9ba1f4511f
drawje <drawje@naver.com>
2025-02-10 21:07:23 +0900

    feat(AppConfig): config 파일 생성 - config 파일로 의존성 주입

--- src/main/java/racingcar/AppConfig.java
+++ src/main/java/racingcar/AppConfig.java
@@ -1,4 +1,26 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+import racingcar.model.RacingGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class AppConfig {
+    public InputView inputView(){
+        return new InputView();
+    }
+    public OutputView outputView(){
+        return new OutputView();
+    }
+    public RacingGame racingGame(){
+        return new RacingGame();
+    }
+    public RacingGameController racingGameController(){
+        return new RacingGameController(
+                this.inputView(),
+                this.outputView(),
+                this.racingGame()
+        );
+    }
 }
+
--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,4 +1,21 @@
 package racingcar.controller;
 
+import racingcar.model.RacingGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class RacingGameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RacingGame racingGame;
+
+    public RacingGameController(InputView inputView, OutputView outputView, RacingGame racingGame) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.racingGame = racingGame;
+    }
+
+    public void run() {
+
+    }
 }
