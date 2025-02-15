2a736e5d2bb178153e73934978d600f2e7fae6e6
yeunsuh <yeunsuh.kim@gmail.com>
2025-02-15 14:43:00 +0900

    feat(main): #f5 finished MVC::model
    created model & custom observer

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,36 @@
 package racingcar.model;
 
+import racingcar.util.ErrorMessage;
+
 public class Car {
+    private String name;
+    private int position;
+    private CarObserver observer;
+
+    public Car(String name){
+        setName(name);
+        this.position = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.length()>5) throw new IllegalArgumentException(ErrorMessage.TOOLONG.getMessage());
+        this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void addPosition(int dice) {
+        if(dice>3) this.position++;
+        observer.onCarMove(name, position);
+    }
+
+    public void setObserver(CarObserver observer) {
+        this.observer = observer;
+    }
 }
