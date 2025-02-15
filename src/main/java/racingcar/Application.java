5f1616e15e2f2fdc96edec294bf08b26af125690
yeunsuh <yeunsuh.kim@gmail.com>
2025-02-15 14:52:00 +0900

    feat(main): set Application & properties

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+import racingcar.service.RacingGameService;
+import racingcar.view.RacingGameView;
+
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new Application().run();
+    }
+
+    public void run(){
+        RacingGameService racingGameService = new RacingGameService();
+        RacingGameView racingGameView = new RacingGameView();
+        RacingGameController racingGameController = new RacingGameController(racingGameView, racingGameService);
+
+        racingGameController.makeCar();
+        racingGameController.runGame();
+        racingGameController.result();
     }
 }
